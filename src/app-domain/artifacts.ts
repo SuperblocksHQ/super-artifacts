45b914a6a91247ed8cbc6b5beeb8345a4d4a20e7
donfrigo <donfrigo@gmail.com>
2020-02-26 11:23:40 +0100

    removed console.log & renamed function

--- src/app-domain/artifacts.ts
+++ src/app-domain/artifacts.ts
@@ -11,21 +11,20 @@ export class Artifacts {
     }
 
     static createNew(deploymentId: string, artifacts: ITruffleBuild[]) {
-        console.log(artifacts);
         if (!deploymentId || !artifacts) {
             throw new Error('Invalid parameters while creating artifacts');
         }
 
         return new Artifacts({
            deploymentId,
-           artifacts: narrowArtifactsObject(artifacts)
+           artifacts: reduceArtifactsObject(artifacts)
         });
     }
 
     get persisted(): IPersistedArtifactsModel { return this.persistedModel; }
 }
 
-function narrowArtifactsObject(artifacts: ITruffleBuild[]) {
+function reduceArtifactsObject(artifacts: ITruffleBuild[]) {
     return artifacts.map((artifactsObj: ITruffleBuild) => {
         const { contractName, abi, metadata, bytecode, sourceMap } = artifactsObj;
         return { contractName, abi, metadata, bytecode, sourceMap };
