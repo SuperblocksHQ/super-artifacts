c87733b5e290a2428d9c398e2abb28cae14a33d1
donfrigo <donfrigo@gmail.com>
2020-02-26 11:24:17 +0100

    added console.log

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { superblocksClient } from './infrastructure-superblocks';
 async function harvestArtifacts(config: ITruffleConfig, done: () => void) {
     const command = new HarvestArtifactsCommand(truffle, superblocksClient);
     await command.execute(config);
+    console.log('[Superblocks Artifacts] Deployment Artifacts have been saved!');
     done();
 }
 
