0a5222235bca987208adc168b7d385e87b0dda55
donfrigo <donfrigo@gmail.com>
2020-02-26 11:23:49 +0100

    fix style

--- src/app-domain/models/superblocks/artifacts.ts
+++ src/app-domain/models/superblocks/artifacts.ts
@@ -1,11 +1,11 @@
 import { IABI } from '../truffle';
 
 export interface IArtifactsModel {
-    contractName:      string;
-    abi:               IABI[];
-    metadata:          string;
-    bytecode:          string;
-    sourceMap:         string;
+    contractName: string;
+    abi: IABI[];
+    metadata: string;
+    bytecode: string;
+    sourceMap: string;
 }
 
 export interface IPersistedArtifactsModel {
